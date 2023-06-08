61b0794b77384e966f70eb336be5c6e9a77998fd
yih117 <yih117@ucsd.edu>
2023-06-08 11:21:22 +0800

    Update vrp.py

--- tasks/vrp.py
+++ tasks/vrp.py
@@ -142,7 +142,9 @@ class VehicleRoutingDataset(Dataset):
             all_demands[depot.nonzero().squeeze(), 0] = 0.
 
         tensor = torch.cat((all_loads.unsqueeze(1), all_demands.unsqueeze(1)), 1)
-        cumulative_reward = torch.sum(all_loads.unsqueeze(1)[:,1:] - dynamic[:, 0][:,1],axis=1)
+        print(tensor.shape)
+        print(dynamic.shape)
+        cumulative_reward = torch.sum(tensor[:,0][:,1] - dynamic[:, 0][:,1],axis=1)
         print(cumulative_reward.shape)
         return torch.tensor(tensor.data, device=dynamic.device), cumulative_reward
 
