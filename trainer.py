14d4be67b836a11c85c38ad1cf3eda4c321b6f2c
yih117 <yih117@ucsd.edu>
2023-06-12 14:37:51 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -168,7 +168,7 @@ def train(actor, critic, task, num_nodes, train_data, valid_data, reward_fn,
             reward = reward_fn(static, tour_indices, cumulative_reward, max_time)
 
             # Query the critic for an estimate of the reward
-            critic_est = critic(static, dynamic).view(-1)
+            critic_est = critic(static, dynamic.float()).view(-1)
 
             advantage = (reward - critic_est)
             actor_loss = torch.mean(advantage.detach() * tour_logp.sum(dim=1))
