97e9ade0b9ec83f3457fe2ff0c895858b6076cd4
yih117 <yih117@ucsd.edu>
2023-06-13 11:39:56 +0800

    Update vrp.py

--- tasks/vrp.py
+++ tasks/vrp.py
@@ -202,7 +202,9 @@ def render(static, tour_indices, save_path):
 
     for i, ax in enumerate(axes):
         
-        ax.plot(static[i,0,:].cpu().data.numpy(), static[i,1,:].cpu().data.numpy(), zorder=1, label=0)
+        #ax.plot(static[i,0,:].cpu().data.numpy(), static[i,1,:].cpu().data.numpy(), zorder=1, label=0)
+        for j in range(static.shape[2]):
+            ax.plot(static[i,0,j], static[i,1,j], marker='v', color="white")
 
         # Convert the indices back into a tour
         idx = tour_indices[i]
